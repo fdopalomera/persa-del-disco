5ec077e3d88e4a14c4835c151ffc0e785e9064d8
Fernando Palomera <fernando@Fernandos-MacBook-Pro.local>
2023-06-13 23:52:13 -0400

    update app

--- store/models.py
+++ store/models.py
@@ -1,29 +1,30 @@
 from django.db import models
-from enum import Enum
+
+class ProductCategory(models.Model): #Namespace? Product Category vs Category
+    name = models.CharField(max_length=20)
+
+class ProductType(models.Model):
+    name = models.varchar(max_length=100)
+    product_category_id = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
 
 class Product(models.Model):
 
-    class Category(models.TextChoices):
-        AUDIO = "AUDIO"
-        VIDEO = "VIDEO"
-        MISCELLANEOUS = "MISC"
-
-    class Type(models.TextChoices):
-        CD = 'CD'
-        DVD = 'DVD'
-        BLURAY = 'Blu-ray'
-        LP = 'Vinyl LP'
-
     name = models.CharField(max_length=256)
-    category = models.CharField(max_length=5, choices=Category.choices) # Should have a default?
-    type = models.CharField(max_length=10, choices=Type.choices) # Dependency with category
+    product_category = models. # Shouldn't be present? 3NF
+    product_type = models.ForeignKey # Dependency with category
     price_clp = models.PositiveIntegerField()
-    stock_units = models.PositiveSmallIntegerField() # Max 32767 or should use PositiveInegerField?
-    creation_date = models.DateField()
-    last_update = models.DateField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
 
     def update_stock(self):
         pass
+
+    def get_product(self):
+        pass
+
+class Stock(models.Model):
+    quantity = models.PositiveSmallIntegerField() # Max 32767 or should use PositiveInegerField?
+    cost_clp = models.PositiveIntegerField()
 
 class Order(models.Model):
 
@@ -37,7 +38,12 @@ class OrderItem(models.Model):
     quantity = models.PositiveSmallIntegerField()
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(blank=True, defualt='') # Optional
+    first_last_name = models.CharField(max_length=100)
+    second_last_name = models.CharField(max_length=100)
+    email_adress = models.EmailField()
+    created_at = models.DateTimeField()
+
 
 
